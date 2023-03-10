2766540ecab84286b02f4f7750b1f341e92847ce
MrBHerring <111889897+MrBHerring@users.noreply.github.com>
2023-03-10 18:02:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -56,10 +56,10 @@ function main() {
           let max_int = Number(processNum.trim())
           gbf.set_max_number(max_int)
           let tupples = gbf.run_process();
-          //console.log(max_int + ": " + tupples);
+          console.log('We found %i Goldbach pair(s) for %i', tupples.length, max_int);
           let index = 0;
           while (index < tupples.length){
-            console.log(max_int + ": " + tupples[index]);
+            console.log('%i = %i + %i', max_int, tupples[0], tupples[1]);
             index++;
           }
         });
