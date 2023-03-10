8cc74d1a1c6dd86b0d475867814d10d81a4293c8
MrBHerring <111889897+MrBHerring@users.noreply.github.com>
2023-03-10 19:39:15 +0000

    Reviewed the goldbach.js code.
    It now displays functionality for any integer.

--- libs/goldbach.js
+++ libs/goldbach.js
@@ -21,9 +21,9 @@ class goldbachFormulas {
   /**
    * @property {Function} run_process executes the goldbach formulas in the
    *           class
+   * @return {function} goldbach formula
    */
   run_process() {
-
     return this.__goldbach_formula();
   }
 
@@ -36,8 +36,9 @@ class goldbachFormulas {
   __get_primes(n) {
     // reference this.__max_number
     const x = [];
-    let i, j = 0;
-    let primes = [];
+    let i = 0;
+    let j = 0;
+    const primes = [];
     for (i = 2; i <= n; ++i) {
       if (!x[i]) {
         primes.push(i);
@@ -54,31 +55,29 @@ class goldbachFormulas {
 * @property {Function} __goldbach_formula executes the goldbach formulas in the
 *           class
 *
-*
+*@return {tupple} return tupples
 */
   __goldbach_formula() {
     // reference this.__max_number
-    let count = 0;
+
     const primes = this.__get_primes(this.__max_number);
+    //  console.log(primes)
     const tupples = [];
     const len = primes.length;
-    console.log(len);
+    //  console.log(len);
     for (let i = 0; i < len; i++) {
-      let k = 0;
       for (let j = len - 1; j >= i; j--) {
-        console.log(primes[i] + ": " + primes[j]);
+        //  console.log(j)
+        // console.log(primes[i] + ": " + primes[j]);
         if (primes[i] + primes[j] == this.__max_number) {
           tupples.push(primes[i] + ' + ' + primes[j]);
-          console.log(tupples);
-          count++;
-          k=j;
-        
+          //  console.log(tupples);
+          //  count++;
+          //  console.log(j)
         }
       }
-      if (primes[i] + primes[k] === this.__max_number) {
-        break;
-      }
     }
+
     return tupples;
   }
 }
